8dbe0ea59cfedc38225cfb586b86d21ca261fb4a
Kristina Zbinden <53029467+kristinazbinden@users.noreply.github.com>
2019-12-17 12:09:23 -0800

    add changes

--- controllers/issues.php
+++ controllers/issues.php
@@ -1,6 +1,6 @@
 <?php
 header('Content-Type: application/json');
-header('Access-Control-Allow-Origin: *');
+// header('Access-Control-Allow-Origin: *');
 header("Access-Control-Allow-Headers: X-API-KEY, Origin, X-Requested-With, Content-Type, Accept, Access-Control-Request-Method");
 header("Access-Control-Allow-Methods: GET, POST, OPTIONS, PUT, DELETE"); //sending back json
 include_once __DIR__ . '/../models/issue.php'; //include what is in the models directory up out the controllers dir and into models
